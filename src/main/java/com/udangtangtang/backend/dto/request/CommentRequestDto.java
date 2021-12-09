e7f39bb2eaa4e2c3d1adac4e5122fa4fbc7fbe91
KKHoon210417 <zozo1591@naver.com>
2021-12-09 17:13:21 +0900

    API docs 작성 #48

--- src/main/java/com/udangtangtang/backend/dto/request/CommentRequestDto.java
+++ src/main/java/com/udangtangtang/backend/dto/request/CommentRequestDto.java
@@ -1,7 +1,10 @@
 package com.udangtangtang.backend.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class CommentRequestDto {
     private String commentText;
