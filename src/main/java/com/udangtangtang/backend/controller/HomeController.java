3c2b74b72cdf511494bae6675006a488b229089e
profoundsea25 <profoundsea25@gmail.com>
2021-12-21 22:57:41 +0900

    Slack 상태관리 알림#94

--- src/main/java/com/udangtangtang/backend/controller/HomeController.java
+++ src/main/java/com/udangtangtang/backend/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.udangtangtang.backend.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping(value = "/")
-    public String forHealthCheck () {
+    public String forHealthCheck(Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/error/test/CyhDZOquoh")
+    public String forErrorTest() {
+        int i = 1/0;
+        return "FOR ERROR TEST";
     }
 }
